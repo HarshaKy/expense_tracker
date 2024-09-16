00fb9469520a163a2b9d440bb14cbcfd33ae2354
HarshaKy <harshaky6573@gmail.com>
2024-09-16 20:41:40 +0100

    modal setup to add expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 import 'package:flutter/material.dart';
 
@@ -32,9 +33,26 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _openAddExpenseOverlay,
+          ),
+        ],
+        // backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: Column(
         children: [
           const Text('The chart'),
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            // keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              // labelText: 'Title',
+              label: Text('Title'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
